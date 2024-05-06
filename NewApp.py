c60b140fe8fa4afedab771312d20851dc103534e
rajibash <168216370+rajibash@users.noreply.github.com>
2024-05-06 22:00:46 +0100

    Add files via upload

---
+++ NewApp.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+import pandas as pd
+
+import calendar
+
+from datetime import date
+
+todays_date = date.today()
+
+st.set_page_config(page_title="Daily Hotel Sales",
+                   page_icon="🖥")
+
+
+st.subheader("Hotel Sales Record")
+
+
+
+with st.form(key="form", clear_on_submit=True):
+    date = todays_date = date.today()
+    roomNumber = st.text_input('Enter the room number')
+    amount = st.number_input('Enter the amount paid')
+    add_btn = st.form_submit_button("Add", type="primary")
+
+
+Sales = 0
+
+    # Event handler
+def SalesCalculator(roomNumber, amount):
+        if roomNumber and amount:
+            Sales =+ amount
+            Sales = round(Sales,2)
+            print(f"The total sales for {todays_date} is {Sales}")
+            return f"The total sales for {todays_date} is {Sales}"
+            
+
+
+if add_btn:
+        display = SalesCalculator(roomNumber=roomNumber, amount=amount)
+        st.subheader(display)
